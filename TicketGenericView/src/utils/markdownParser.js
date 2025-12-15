eea6e926934cc24c3ce2252fa1e977c74c76caa4
HMS-Victory <90852625+HMS-Victory@users.noreply.github.com>
2025-12-15 07:51:30 -0700

    fix: change replace to replace all for every global regex check in markdownParser

--- TicketGenericView/src/utils/markdownParser.js
+++ TicketGenericView/src/utils/markdownParser.js
@@ -4,40 +4,39 @@ export const parseMarkdown = (markdown) => {
   let html = markdown;
 
   // Escape HTML characters first
-  html = html.replace('&', '&amp;')
-    .replace('<', '&lt;')
-    .replace('>', '&gt;');
+  html = html.replaceAll(/&/g, '&amp;')
+    .replaceAll(/</g, '&lt;')
+    .replaceAll(/>/g, '&gt;');
 
   // Headers - now more flexible with optional space after #
-  html = html.replace(/^###\s*([^\\n]+)$/gim, '<h3>$1</h3>');
-  html = html.replace(/^##\s*([^\\n]+)$/gim, '<h2>$1</h2>');
-  html = html.replace(/^#\s*([^\\n]+)$/gim, '<h1>$1</h1>');
+  html = html.replaceAll(/^###\s*(.*$)/gim, '<h3>$1</h3>');
+  html = html.replaceAll(/^##\s*(.*$)/gim, '<h2>$1</h2>');
+  html = html.replaceAll(/^#\s*(.*$)/gim, '<h1>$1</h1>');
 
   // Bold and italic
-  html = html.replace(/\*\*\*(.+?)\*\*\*/g, '<strong><em>$1</em></strong>');
-  html = html.replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>');
-  html = html.replace(/\*(.+?)\*/g, '<em>$1</em>');
-  html = html.replace(/___(.+?)___/g, '<strong><em>$1</em></strong>');
-  html = html.replace(/__(.+?)__/g, '<strong>$1</strong>');
-  html = html.replace(/_(.+?)_/g, '<em>$1</em>');
+  html = html.replaceAll(/\*\*\*(.*?)\*\*\*/g, '<strong><em>$1</em></strong>');
+  html = html.replaceAll(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
+  html = html.replaceAll(/\*(.*?)\*/g, '<em>$1</em>');
+  html = html.replaceAll(/\_\_\_(.*?)\_\_\_/g, '<strong><em>$1</em></strong>');
+  html = html.replaceAll(/\_\_(.*?)\_\_/g, '<strong>$1</strong>');
+  html = html.replaceAll(/\_(.*?)\_/g, '<em>$1</em>');
 
   // Links
-  // Links
-html = html.replaceAll(/$$([^$$]+)\]$$([^)]+)$$/g, '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>');
+  html = html.replaceAll(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2" target="_blank" rel="noopener noreferrer">$1</a>');
 
   // Code blocks and inline code
-  html = html.replace(/```([a-z]*)\n([\s\S]*?)```/g, '<pre><code class="language-$1">$2</code></pre>');
-  html = html.replace(/`([^`]+)`/g, '<code>$1</code>');
+  html = html.replaceAll(/```([a-z]*)\n([\s\S]*?)```/g, '<pre><code class="language-$1">$2</code></pre>');
+  html = html.replaceAll(/`([^`]+)`/g, '<code>$1</code>');
 
   // Lists
-  html = html.replace(/^\* (.+)$/gim, '<li>$1</li>');
-  html = html.replace(/^\- (.+)$/gim, '<li>$1</li>');
-  html = html.replace(/^\+ (.+)$/gim, '<li>$1</li>');
+  html = html.replaceAll(/^\* (.+)$/gim, '<li>$1</li>');
+  html = html.replaceAll(/^\- (.+)$/gim, '<li>$1</li>');
+  html = html.replaceAll(/^\+ (.+)$/gim, '<li>$1</li>');
   html = html.replace(/(<li>.*<\/li>)/s, '<ul>$1</ul>');
 
-  html = html.replace(/^\d+\. (.+)$/gim, '<li>$1</li>');
+  html = html.replaceAll(/^\d+\. (.+)$/gim, '<li>$1</li>');
   const orderedListRegex = /(<li>.*?<\/li>)(?=\n\d+\.|\n*$)/gs;
-  html = html.replace(orderedListRegex, (match) => {
+  html = html.replaceAll(orderedListRegex, (match) => {
     if (!match.includes('<ul>') && !match.includes('<ol>')) {
       return '<ol>' + match + '</ol>';
     }
@@ -45,38 +44,38 @@ html = html.replaceAll(/$$([^$$]+)\]$$([^)]+)$$/g, '<a href="$2" target="_blank"
   });
 
   // Blockquotes
-  html = html.replace(/^&gt; (.+)$/gim, '<blockquote>$1</blockquote>');
+  html = html.replaceAll(/^&gt; (.+)$/gim, '<blockquote>$1</blockquote>');
 
   // Horizontal rules
-  html = html.replace(/^\-\-\-$/gim, '<hr>');
-  html = html.replace(/^\*\*\*$/gim, '<hr>');
+  html = html.replaceAll(/^\-\-\-$/gim, '<hr>');
+  html = html.replaceAll(/^\*\*\*$/gim, '<hr>');
 
   // Convert single newlines to <br> and double newlines to paragraphs
-  html = html.replace('\n', '<br>');
-  html = html.replace('<br><br>', '</p><p>');
+  html = html.replaceAll(/\n/g, '<br>');
+  html = html.replaceAll(/<br><br>/g, '</p><p>');
   html = '<p>' + html + '</p>';
 
   // Clean up empty paragraphs and fix block elements inside paragraphs
-  html = html.replace('<p><p>', '');
-  html = html.replace(/<p>(<h[1-6]>)/g, '$1');
-  html = html.replace(/(<\/h[1-6]>)<\/p>/g, '$1');
-  html = html.replace(/<p>(<ul>)/g, '$1');
-  html = html.replace(/(<\/ul>)<\/p>/g, '$1');
-  html = html.replace(/<p>(<ol>)/g, '$1');
-  html = html.replace(/(<\/ol>)<\/p>/g, '$1');
-  html = html.replace(/<p>(<pre>)/g, '$1');
-  html = html.replace(/(<\/pre>)<\/p>/g, '$1');
-  html = html.replace(/<p>(<blockquote>)/g, '$1');
-  html = html.replace(/(<\/blockquote>)<\/p>/g, '$1');
-  html = html.replace(/<p>(<hr>)<\/p>/g, '$1');
+  html = html.replaceAll(/<p><\/p>/g, '');
+  html = html.replaceAll(/<p>(<h[1-6]>)/g, '$1');
+  html = html.replaceAll(/(<\/h[1-6]>)<\/p>/g, '$1');
+  html = html.replaceAll(/<p>(<ul>)/g, '$1');
+  html = html.replaceAll(/(<\/ul>)<\/p>/g, '$1');
+  html = html.replaceAll(/<p>(<ol>)/g, '$1');
+  html = html.replaceAll(/(<\/ol>)<\/p>/g, '$1');
+  html = html.replaceAll(/<p>(<pre>)/g, '$1');
+  html = html.replaceAll(/(<\/pre>)<\/p>/g, '$1');
+  html = html.replaceAll(/<p>(<blockquote>)/g, '$1');
+  html = html.replaceAll(/(<\/blockquote>)<\/p>/g, '$1');
+  html = html.replaceAll(/<p>(<hr>)<\/p>/g, '$1');
   
   // Remove <br> tags before and after block elements
-  html = html.replace(/<br>(<h[1-6]>)/g, '$1');
-  html = html.replace(/(<\/h[1-6]>)<br>/g, '$1');
-  html = html.replace(/<br>(<ul>)/g, '$1');
-  html = html.replace(/(<\/ul>)<br>/g, '$1');
-  html = html.replace(/<br>(<ol>)/g, '$1');
-  html = html.replace(/(<\/ol>)<br>/g, '$1');
+  html = html.replaceAll(/<br>(<h[1-6]>)/g, '$1');
+  html = html.replaceAll(/(<\/h[1-6]>)<br>/g, '$1');
+  html = html.replaceAll(/<br>(<ul>)/g, '$1');
+  html = html.replaceAll(/(<\/ul>)<br>/g, '$1');
+  html = html.replaceAll(/<br>(<ol>)/g, '$1');
+  html = html.replaceAll(/(<\/ol>)<br>/g, '$1');
 
   return html;
 };
